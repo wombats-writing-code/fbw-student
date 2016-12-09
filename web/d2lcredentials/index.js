300f32823d6bc74c67be25bbdf07edbb4d734ab7
cjshaw <cjshaw@mit.edu>
2016-12-09 12:47:59 -0500

    add logic for web d2l credentials to inspect hostname

--- web/d2lcredentials/index.js
+++ web/d2lcredentials/index.js
@@ -2,5 +2,10 @@
 let devCredentials = require('./_dev.credentials');
 let prodCredentials = require('./_prod.credentials');
 
-module.exports = devCredentials;
-//module.exports = prodCredentials
+if (location.host === 'fbw-student.mit.edu') {
+  // console.log('prod credentials')
+  module.exports = prodCredentials
+} else {
+  // console.log('dev credentials')
+  module.exports = devCredentials;
+}
