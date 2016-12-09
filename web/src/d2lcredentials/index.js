7652ba4363c160d366e4383685e97c1f3f75864f
cjshaw <cjshaw@mit.edu>
2016-12-09 13:11:52 -0500

    move d2lcredentials directory so webpack can find it

--- web/src/d2lcredentials/index.js
+++ web/src/d2lcredentials/index.js
@@ -4,7 +4,7 @@ let prodCredentials = require('./_prod.credentials');
 
 if (location.host === 'fbw-student.mit.edu') {
   // console.log('prod credentials')
-  module.exports = prodCredentials
+  module.exports = prodCredentials;
 } else {
   // console.log('dev credentials')
   module.exports = devCredentials;
