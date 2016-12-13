368a01ec1315cd4fb62a43676f3d4f848b0bac42
cjshaw <cjshaw@mit.edu>
2016-12-13 11:51:51 -0500

    revert to getting questions for a taken up front

--- web/src/routes/Mission/components/carousel/DirectiveCarousel.js
+++ web/src/routes/Mission/components/carousel/DirectiveCarousel.js
@@ -10,15 +10,13 @@ import './DirectiveCarousel.scss'
 class DirectiveCarousel extends Component {
 
   _renderThumb = (directive, idx) => {
-    // TODO: cjshaw needs to figure out another way to implement this,
-    //   if we go forward with only getting sections without questions.
-    // let outcomeTargets = _.filter(this.props.targets, (t) => t.learningObjectiveIds.indexOf(directive.learningObjectiveId) > -1);
+    let outcomeTargets = _.filter(this.props.targets, (t) => t.learningObjectiveIds.indexOf(directive.learningObjectiveId) > -1);
     let outcome = _.find(this.props.outcomes, {id: directive.learningObjectiveId})
-    //
+    
     let image;
-    // if (hasAchievedDirective(outcomeTargets)) {
-    //   image = <p className="carousel-thumb__icon">&#x02713;</p>
-    // }
+    if (hasAchievedDirective(outcomeTargets)) {
+      image = <p className="carousel-thumb__icon">&#x02713;</p>
+    }
 
     let displayName = outcome ? outcome.displayName.text : 'Error. Somehow this outcome is undefined';
 
@@ -26,11 +24,7 @@ class DirectiveCarousel extends Component {
     let thumb = (
       <div key={idx}
           className={isActive ? "carousel-thumb is-active carousel-thumb--directive" : "carousel-thumb carousel-thumb--directive"}>
-        <button className="carousel-thumb__button" onClick={() => this.props.onSelectDirective({
-          directiveIndex: idx,
-          mission: this.props.mission,
-          sectionId: directive.id,
-          username: this.props.username})}
+        <button className="carousel-thumb__button" onClick={() => this.props.onSelectDirective(idx)}
                 aria-label={`Learning Outcome: ${displayName}`}>
           <div className="flex-container align-center space-between">
             {image}
