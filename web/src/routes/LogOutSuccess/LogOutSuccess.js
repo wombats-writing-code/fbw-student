1286063da444efa3c0618573f2244061dfbeb55b
luwen-huang <huang.luwen@gmail.com>
2017-02-28 20:23:24 -0500

    stylistic changes to guid

--- web/src/routes/LogOutSuccess/LogOutSuccess.js
+++ web/src/routes/LogOutSuccess/LogOutSuccess.js
@@ -10,11 +10,11 @@ class LogOutSuccess extends Component {
       <div className="logout-success flex-container justify-center align-center">
         <div className="logout-success__wrapper">
           <p className="logout-success__prompt text-center">
-            You must also log out from your D2L account. Go to:
+            You <b>must</b> also log out from your D2L account. Go to:
           </p>
           <p><a className="acc-link" href="https://acc.desire2learn.com/d2l/home">My ACC D2L Portal &rarr;</a></p>
           <p className="logout-success__text text-center">
-            If you don't log out from there, other people will be able to see your account here on FbW.
+            If you don't log out from there, other people will be able to see your missions here.
           </p>
         </div>
       </div>)
