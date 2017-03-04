8038d8ba3c1c6861fa6223ebb8af7fedb52adf90
luwen-huang <huang.luwen@gmail.com>
2017-03-04 14:06:32 -0500

    vbump, guest

--- web/src/routes/Home/components/Home.js
+++ web/src/routes/Home/components/Home.js
@@ -14,29 +14,10 @@ const Courses = CoursesContainer(CoursesComponent)
 class Home extends Component {
 
   componentDidMount() {
-    if (this.props.currentCourse) {
-      this.props.getMissions({
-        course: this.props.currentCourse,
-        user: this.props.user
-      })
-    } else {
-      // browserHistory.push('/login')
-    }
   }
 
   render() {
-
     if (!this.props.user) return null;
-
-    let nextActionPrompt;
-    if (this.props.missions && this.props.missions.length > 0) {
-      nextActionPrompt = (
-        <p className="next-action-prompt">
-          Go to your class to begin.
-        </p>
-      )
-    }
-
 
     return (
       <div className="home row">
@@ -45,7 +26,10 @@ class Home extends Component {
             <span className="greeting">Welcome</span>,
             <span className="name"> {getD2LDisplayName(this.props.user)}</span>
           </h1>
-          {nextActionPrompt}
+
+          <p className="next-action-prompt">
+            Go to your class to begin.
+          </p>
 
           <Courses />
 
