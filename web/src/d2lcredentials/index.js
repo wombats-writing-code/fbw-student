b0f6b18569d902270a0aead67b6b7e7f6e597d17
cjshaw <cjshaw@mit.edu>
2017-03-10 13:07:29 -0500

    fix d2l credentials to use env vars

--- web/src/d2lcredentials/index.js
+++ web/src/d2lcredentials/index.js
@@ -1,11 +1,10 @@
-
-let devCredentials = require('./_dev.credentials');
-let prodCredentials = require('./_prod.credentials');
-
-if (location.host === 'fbw-student.mit.edu') {
-  // console.log('prod credentials')
-  module.exports = prodCredentials;
-} else {
-  // console.log('dev credentials')
-  module.exports = devCredentials;
+let credentials = {
+  role: 'student',
+  appID: process.env.PROD_D2L_APP_ID || process.env.DEV_D2L_APP_ID || '',
+  appKey: process.env.PROD_D2L_APP_KEY || process.env.DEV_D2L_APP_KEY || '',
+  host: process.env.D2L_HOST || 'http:localhost:8888/mock-d2l',
+  port: process.env.D2L_PORT || 80,     // so that the valence lib doesn't append a :port to the string.
+  callbackUrl: process.env.D2L_CALLBACK || 'http:localhost:3000'
 }
+
+module.exports = credentials
