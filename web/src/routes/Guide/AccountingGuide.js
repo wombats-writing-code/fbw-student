16c895764f82e37c93065e090feeedc57079d4d7
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-08-28 09:26:33 -0400

    add alt-text to the help guide images

--- web/src/routes/Guide/AccountingGuide.js
+++ web/src/routes/Guide/AccountingGuide.js
@@ -19,12 +19,21 @@ const AlgebraGuide = (props) => {
               <p>Click on the <b>Arapahoe MyACC</b> button</p>
               <p>It should take you to the MyACC screen. Login with your MyACC info.</p>
               <div className="row">
-                <img className="medium-7 columns" src={require("./assets/login-1.png")} />
-                <img className="medium-5 columns" src={require("./assets/login-2.png")} />
+                <img
+                  alt="Fly-by-wire ACC login link"
+                  className="medium-7 columns"
+                  src={require("./assets/login-1.png")} />
+                <img
+                  alt="My ACC login page"
+                  className="medium-5 columns"
+                  src={require("./assets/login-2.png")} />
               </div>
               <p>It will redirect you back to Fly-by-Wire...wait for it...</p>
               <div className="row">
-                <img className="medium-8 columns" src={require("./assets/login-3.png")} />
+                <img
+                  alt="Redirecting back to the Fly-by-Wire application after ACC login"
+                  className="medium-8 columns"
+                  src={require("./assets/login-3.png")} />
               </div>
             </li>
             <li className="instruction">
@@ -35,8 +44,14 @@ const AlgebraGuide = (props) => {
               </p>
               <p>Click to your Missions.</p>
               <div className="row">
-                <img className="medium-6 columns" src={require("./assets/missions-1.png")}/>
-                <img className="medium-6 columns" src={require("./assets/missions-2.png")}/>
+                <img
+                  alt="Phase I assignment icon and text"
+                  className="medium-6 columns"
+                  src={require("./assets/missions-1.png")}/>
+                <img
+                  alt="Phase II assignment icon and text"
+                  className="medium-6 columns"
+                  src={require("./assets/missions-2.png")}/>
               </div>
               <p>
                 A Mission consists of <span className="">goals</span>.
@@ -45,26 +60,38 @@ const AlgebraGuide = (props) => {
               </p>
 
               <div className="row">
-                <img className="medium-7 columns" src={require("./assets/missions-3--accounting.png")}/>
+                <img
+                  alt="Goals and targets, each in their own horizontal carousel"
+                  className="medium-7 columns"
+                  src={require("./assets/missions-3--accounting.png")}/>
               </div>
             </li>
             <li className="instruction">
               <p>Select a Target to start. Do your best to answer it.</p>
               <p>If you get it right, great! Move on to the next one.</p>
               <div className="row">
-                <img className="medium-6 columns" src={require("./assets/do-question-correct--accounting.png")}/>
+                <img
+                  alt="A target question answered correctly"
+                  className="medium-6 columns"
+                  src={require("./assets/do-question-correct--accounting.png")}/>
               </div>
               <p>If you get it wrong, Fly-by-Wire will tell you why you got it wrong, and give you a next question that addresses your misunderstanding.
                 This is called a <span className="">Waypoint</span> question.
               </p>
               <div className="row">
-                <img className="medium-10 columns" src={require("./assets/do-question-1--accounting.png")}/>
+                <img
+                  alt="A sequence of two questions, showing how a target question answered incorrectly generates a review question"
+                  className="medium-10 columns"
+                  src={require("./assets/do-question-1--accounting.png")}/>
               </div>
               <p>
                 <b>Read the solution explanation</b>.
               </p>
               <div className="row">
-                <img className="medium-8 columns" src={require("./assets/do-question-2--accounting.png")}/>
+                <img
+                  alt="Each answered question has a solution block that you should review"
+                  className="medium-8 columns"
+                  src={require("./assets/do-question-2--accounting.png")}/>
               </div>
               <p>
                 Then, try to answer the next question. If you get it wrong, Fly-by-Wire will give you an easier question.
@@ -74,7 +101,10 @@ const AlgebraGuide = (props) => {
             <li className="instruction">
               <p>When you answer all of the Waypoint questions correctly, you will have <i>navigated</i> the route &mdash; you should now be ready to try the next Target question. </p>
               <div className="row">
-                <img className="columns" src={require("./assets/do-question-3--accounting.png")}/>
+                <img
+                  alt="A fully navigated route will show the 'tried' badge in the target carousel"
+                  className="columns"
+                  src={require("./assets/do-question-3--accounting.png")}/>
               </div>
             </li>
             <li className="instruction">
@@ -82,13 +112,22 @@ const AlgebraGuide = (props) => {
             </li>
             <li className="instruction">
               <p>To log out, click on the <span className="button-ref">Logout</span> button: </p>
-              <img className="" src={require("./assets/logout-button.png")}/>
+              <img
+                alt="The logout button for the Fly-by-Wire application"
+                className=""
+                src={require("./assets/logout-button.png")}/>
 
               <p><b>Important!</b> Then, click on the link to your myACC D2L account:</p>
-              <img className="" src={require("./assets/logout-link.png")}/>
+              <img
+                alt="After logging out of Fly-by-Wire, a link will appear directing you to log out of Desire 2 Learn"
+                className=""
+                src={require("./assets/logout-link.png")}/>
 
               <p>You <b>must</b> logout from your myACC D2L account!</p>
-              <img className="" src={require("./assets/logout-d2l.png")}/>
+              <img
+                alt="Log out of the Desire 2 Learn application as well"
+                className=""
+                src={require("./assets/logout-d2l.png")}/>
             </li>
           </ol>
         </div>
--- web/src/routes/Guide/AlgebraGuide.js
+++ web/src/routes/Guide/AlgebraGuide.js
@@ -19,12 +19,21 @@ const AlgebraGuide = (props) => {
               <p>Click on the <b>Arapahoe MyACC</b> button</p>
               <p>It should take you to the MyACC screen. Login with your MyACC info.</p>
               <div className="row">
-                <img className="medium-7 columns" src={require("./assets/login-1.png")} />
-                <img className="medium-5 columns" src={require("./assets/login-2.png")} />
+                <img
+                  alt="Fly-by-wire ACC login link"
+                  className="medium-7 columns"
+                  src={require("./assets/login-1.png")} />
+                <img
+                  alt="My ACC login page"
+                  className="medium-5 columns"
+                  src={require("./assets/login-2.png")} />
               </div>
               <p>It will redirect you back to Fly-by-Wire...wait for it...</p>
               <div className="row">
-                <img className="medium-8 columns" src={require("./assets/login-3.png")} />
+                <img
+                  alt="Redirecting back to the Fly-by-Wire application after ACC login"
+                  className="medium-8 columns"
+                  src={require("./assets/login-3.png")} />
               </div>
             </li>
             <li className="instruction">
@@ -35,8 +44,14 @@ const AlgebraGuide = (props) => {
               </p>
               <p>Click to your Missions.</p>
               <div className="row">
-                <img className="medium-6 columns" src={require("./assets/missions-1.png")}/>
-                <img className="medium-6 columns" src={require("./assets/missions-2.png")}/>
+                <img
+                  alt="Phase I assignment icon and text"
+                  className="medium-6 columns"
+                  src={require("./assets/missions-1.png")}/>
+                <img
+                  alt="Phase II assignment icon and text"
+                  className="medium-6 columns"
+                  src={require("./assets/missions-2.png")}/>
               </div>
               <p>
                 A Mission consists of <span className="">goals</span>.
@@ -45,7 +60,10 @@ const AlgebraGuide = (props) => {
               </p>
 
               <div className="row">
-                <img className="medium-7 columns" src={require("./assets/missions-3.png")}/>
+                <img
+                  alt="Goals and targets, each in their own horizontal carousel"
+                  className="medium-7 columns"
+                  src={require("./assets/missions-3.png")}/>
               </div>
             </li>
             <li className="instruction">
@@ -53,20 +71,29 @@ const AlgebraGuide = (props) => {
               <p><b>Write down all of your work</b> as you attempt the problem as this is what is expected on exams.</p>
               <p>If you get it right, great! Move on to the next one.</p>
               <div className="row">
-                <img className="medium-6 columns" src={require("./assets/do-question-correct.png")}/>
+                <img
+                  alt="A target question answered correctly"
+                  className="medium-6 columns"
+                  src={require("./assets/do-question-correct.png")}/>
               </div>
               <p>If you get it wrong, Fly-by-Wire will tell you why you got it wrong, and give you a next question that addresses your misunderstanding.
                 This is called a <span className="bold">Waypoint</span> question.
               </p>
               <div className="row">
-                <img className="medium-8 columns" src={require("./assets/do-question-1.png")}/>
+                <img
+                  alt="A sequence of two questions, showing how a target question answered incorrectly generates a review question"
+                  className="medium-8 columns"
+                  src={require("./assets/do-question-1.png")}/>
               </div>
               <p>
                 <b>Read the solution explanation</b>.
                 Compare the solution to your written work and see if you can pinpoint your error.
               </p>
               <div className="row">
-                <img className="medium-10 columns" src={require("./assets/do-question-2.png")}/>
+                <img
+                  alt="Each answered question has a solution block that you should review"
+                  className="medium-10 columns"
+                  src={require("./assets/do-question-2.png")}/>
               </div>
               <p>
                 Then, try to answer the next question. If you get it wrong, Fly-by-Wire will give you an easier question.
@@ -76,7 +103,10 @@ const AlgebraGuide = (props) => {
             <li className="instruction">
               <p>When you answer all of the Waypoint questions correctly, you will have <i>navigated</i> the route &mdash; you should now be ready to try the next Target question. </p>
               <div className="row">
-                <img className="medium-6 columns" src={require("./assets/do-question-3.png")}/>
+                <img
+                  alt="A fully navigated route will show the 'tried' badge in the target carousel"
+                  className="medium-6 columns"
+                  src={require("./assets/do-question-3.png")}/>
               </div>
             </li>
             <li className="instruction">
@@ -84,13 +114,22 @@ const AlgebraGuide = (props) => {
             </li>
             <li className="instruction">
               <p>To log out, click on the <span className="button-ref">Logout</span> button: </p>
-              <img className="" src={require("./assets/logout-button.png")}/>
+              <img
+                alt="The logout button for the Fly-by-Wire application"
+                className=""
+                src={require("./assets/logout-button.png")}/>
 
               <p><b>Important!</b> Then, click on the link to your myACC D2L account:</p>
-              <img className="" src={require("./assets/logout-link.png")}/>
+              <img
+                alt="After logging out of Fly-by-Wire, a link will appear directing you to log out of Desire 2 Learn"
+                className=""
+                src={require("./assets/logout-link.png")}/>
 
               <p>You <b>must</b> logout from your myACC D2L account!</p>
-              <img className="" src={require("./assets/logout-d2l.png")}/>
+              <img
+                alt="Log out of the Desire 2 Learn application as well"
+                className=""
+                src={require("./assets/logout-d2l.png")}/>
             </li>
 
           </ol>
