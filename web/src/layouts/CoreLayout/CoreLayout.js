0ddd7fa77bd1d7df347f3708befc5e13d5bde3d2
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-09-14 15:00:58 -0400

    add LiveAnnouncer

--- web/src/layouts/CoreLayout/CoreLayout.js
+++ web/src/layouts/CoreLayout/CoreLayout.js
@@ -2,6 +2,7 @@ import '../../styles/foundation.min.css'
 
 import React from 'react'
 import NavBar from 'fbw-platform-common/components/nav-bar/web'
+import { LiveAnnouncer } from 'react-aria-live'
 
 import './CoreLayout.scss'
 import '../../styles/core.scss'
@@ -21,12 +22,14 @@ export const CoreLayout = ({ children }) => {
 
   return (
     <div className='container text-center' style={{height: '100%', width: '100%'}}>
-      <NavBar {...children.props}/>
-      <div className='core-layout__viewport'>
-        {children}
-        {feedbackWidget}
-        {/* {outcomeVisual} */}
-      </div>
+      <LiveAnnouncer>
+        <NavBar {...children.props}/>
+        <div className='core-layout__viewport'>
+          {children}
+          {feedbackWidget}
+          {/* {outcomeVisual} */}
+        </div>
+      </LiveAnnouncer>
     </div>
   )
 }
